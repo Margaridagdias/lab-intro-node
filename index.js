76bfd85a4a07b23df463b3b643a6c3c83da96ed8
Margarida <margarida97pt@gmail.com>
2020-11-02 17:24:18 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    
+    this.items.sort((a, b) => {
+      return a - b;
+    });
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length <= pos) {
+    throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+  if (this.items.length === 0) {
+    throw new Error('EmptySortedList');
+  } else {
+    return Math.max(...this.items);
+  }
+}
+   
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  sum() {
+  if (this.items.length === 0) {
+  return 0;
+  } else {
+  return this.items.reduce((a,b) => {
+      return a+b
+    })
+  }
 }
 
+
+  avg() {
+    if (this.items.length === 0) {
+    throw new Error('EmptySortedList');
+    } else {
+        return this.sum() /this.items.length
+      }
+    }
+  }
+
+
 module.exports = SortedList;
+
